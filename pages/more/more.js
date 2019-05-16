4b61840ff4d744d5041fcefb8859fd260c98c7ac
shirealson <562631268@qq.com>
2019-05-16 23:31:12 +0800

    可以获取头像了

--- pages/more/more.js
+++ pages/more/more.js
@@ -9,6 +9,9 @@ Page({
     data: {
         url: "", //请求地址
         openid: "more_openid",
+        hasUserInfo: false,
+        pic_num : 0,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
         search_value: "",
         img_title: "",
         image_array: "",//存放图片数组
@@ -56,13 +59,42 @@ Page({
                     });
                 } else {
                     that.setData({
-                        image_array: result.data //显示无图信息
+                        image_array: result.data, //显示无图信息
+                        pic_num : result.data.length
                     });
                 }
                 wx.hideLoading();
 
             }
-        })
+        });
+
+        //----获取头像授权部分
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
 
     },
 
@@ -137,7 +169,8 @@ Page({
                     });
                 } else {
                     that.setData({
-                        image_array: result.data //显示无图信息
+                        image_array: result.data, //显示无图信息
+                        pic_num: result.data.length
                     });
                 }
                 wx.hideLoading();
@@ -371,6 +404,15 @@ Page({
             tipSwitch:false
         });//关闭窗口
         this.refreshImage();//刷新
+    },
+
+    getUserInfo: function (e) {//获取头像授权
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
     }
 
 })
